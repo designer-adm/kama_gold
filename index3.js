947fc18bc46079e501e2f8a98cfa2f2484bf983c
Bikramjit Singh <amanramgharia914@gmail.com>
2024-08-15 15:30:19 +0530

    updated html & js

--- index3.js
+++ index3.js
@@ -6,16 +6,16 @@ document.querySelectorAll('a.scrollToContainer').forEach(anchor => {
     });
 });
 
-document.querySelector('#scrollToContainer').addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent the default anchor behavior
+// document.querySelector('#scrollToContainer').addEventListener('click', function(event) {
+//     event.preventDefault(); // Prevent the default anchor behavior
 
-    // Use querySelector to find the first element with class 'form__wrap'
-    const targetElement = document.querySelector('.form__wrap');
+//     // Use querySelector to find the first element with class 'form__wrap'
+//     const targetElement = document.querySelector('.form__wrap');
     
-    if (targetElement) {
-        // Scroll smoothly to the target element
-        targetElement.scrollIntoView({ behavior: 'smooth' });
-    } else {
-        console.error('Target element with class "form__wrap" not found.');
-    }
-});
+//     if (targetElement) {
+//         // Scroll smoothly to the target element
+//         targetElement.scrollIntoView({ behavior: 'smooth' });
+//     } else {
+//         console.error('Target element with class "form__wrap" not found.');
+//     }
+// });
