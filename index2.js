71811e83b44c95c898ae44126b56a75ba910cc79
Bikramjit Singh <amanramgharia914@gmail.com>
2024-08-15 11:45:41 +0530

    modified js

--- index2.js
+++ index2.js
@@ -1,27 +1,3 @@
-// const scriptURL = 'https://script.google.com/macros/s/AKfycbwnFUJ3aZJphkJYEjKJEq3cdL7T8GqEdANyg7lEbM30fdbOaH4_B0Hnvnhj7vgcevJ59w/exec'
-
-// const form = document.forms['contact-form']
-
-// form.addEventListener('submit', e => {
-//    e.preventDefault()
-//    fetch(scriptURL, {method: 'POST', body: new FormData(form)})
-//    .then(response => {
-//     // Check if the response is successful
-//     if (response.ok) {
-//         // Redirect to the Thank You page
-//         window.location.href = 'thank_you.html';
-//     } else {
-//         throw new Error('Form submission failed');
-//     }
-// })
-// .catch(error => {
-//     console.error('Error!', error.message);
-//     alert('There was a problem submitting the form.');
-// });
-// });
-
-
-
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwnFUJ3aZJphkJYEjKJEq3cdL7T8GqEdANyg7lEbM30fdbOaH4_B0Hnvnhj7vgcevJ59w/exec';
         const form = document.forms['contact-form'];
 
